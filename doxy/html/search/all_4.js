35ac2a6a606b6a92390158758596d04c4d294b9e
danya <99524677+iamnotsorryy@users.noreply.github.com>
2023-06-13 21:56:48 +0300

    Add files via upload

---
+++ doxy/html/search/all_4.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['self_5faddr_0',['self_addr',['../class_server.html#aad6d0eef2397b3368cdd3d6ec134e462',1,'Server']]],
+  ['server_1',['Server',['../class_server.html',1,'']]]
+];
---
+++ doxy/html/search/functions_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['client_5faddr_0',['client_addr',['../class_server.html#a972db953af9cb2c28ec9359788452a5c',1,'Server']]]
+];
